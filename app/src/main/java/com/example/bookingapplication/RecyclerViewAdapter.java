b5cf075c6501d95f690a717e3d972af30fdcb43f
viowla <55015538+viowla@users.noreply.github.com>
2020-11-21 19:22:22 +0600

    add recyclerview adapter edit project build.gradle

---
+++ app/src/main/java/com/example/bookingapplication/RecyclerViewAdapter.java
@@ -0,0 +1,73 @@
+package com.example.bookingapplication;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<String> mText = new ArrayList<>();
+    private ArrayList<String> mImage = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(ArrayList<String> mText, ArrayList<String> mImage, Context mContext) {
+        this.mText = mText;
+        this.mImage = mImage;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_search_results, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mImage.get(position))
+                .into(holder.imageView);
+        holder.textView.setText(mText.get(position));
+
+        holder.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked on the image: "+ mText.get(position));
+                Toast.makeText(mContext, mText.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mText.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView imageView;
+        TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+            textView = itemView.findViewById(R.id.textView);
+        }
+    }
+}
--- app/src/main/java/com/example/bookingapplication/SearchResultsActivity.java
+++ app/src/main/java/com/example/bookingapplication/SearchResultsActivity.java
@@ -1,14 +1,57 @@
 package com.example.bookingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class SearchResultsActivity extends AppCompatActivity {
+
+    private static final String TAG = "SearchResultsActivity";
+
+    private ArrayList<String> mText = new ArrayList<>();
+    private ArrayList<String> mImage = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
+
+        getImages();
+
+    }
+
+    private void getImages(){
+
+        Log.d(TAG, "initImageBitMaps: preparing bitmaps.");
+        
+        mImage.add("https://www.sustainable-bus.com/wp-content/uploads/2019/12/Schermata-2019-12-06-alle-09.59.28.png");
+        mText.add("Almaty-Astana");
+
+        mImage.add("https://www.irizar.com/wp-content/uploads/2019/11/JAZ_869229.jpg");
+        mText.add("Almaty-Astana");
+
+        mImage.add("https://www.sustainable-bus.com/wp-content/uploads/2019/11/img_9935-1-scaled-e1573890391969.jpg");
+        mText.add("Almaty-Astana");
+
+        mImage.add("https://i.pinimg.com/236x/3d/78/08/3d7808b3510dd42025f78373a0936f72--buses-rv.jpg");
+        mText.add("Almaty-Astana");
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        Log.d(TAG, "initRecyclerView: init recyclerview.");
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        RecyclerView recyclerView = findViewById(R.id.listView);
+        recyclerView.setLayoutManager(layoutManager);
+        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(mText, mImage,this);
+        recyclerView.setAdapter(recyclerViewAdapter);
+
     }
 }
